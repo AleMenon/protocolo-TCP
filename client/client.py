695dc5bec2f658bf4da664d57ca944098db99858
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2025-11-16 00:12:40 -0300

    Implementado envio de arquivo e mensagens

--- client/client.py
+++ client/client.py
@@ -1,33 +1,74 @@
 import threading
 import socket
+import hashlib
+import struct
 
 SERVER_IP = 'localhost'
 SERVER_PORT = 8000
 
-nickname = input('Choose a nickname: ')
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER_IP, SERVER_PORT))
 
-def receive():
+def receive(msg_type, file_name=''):
+    try:
+        if msg_type == 'MESSAGE':
+            response = client.recv(4096).decode('ascii')
+            print('Mensagens:')
+            while response != 'OK':
+                print(response)
+                response = client.recv(4096).decode('ascii')
+        else:
+            response = client.recv(64).decode('ascii')
+            if response == 'FILE_NOT_FOUND':
+                raise FileNotFoundError('Arquivo não encontrado pelo servidor')
+
+            expected_hash = client.recv(64).decode()
+            size = struct.unpack('!Q', client.recv(8))[0]
+
+            received = b''
+            while len(received) < size:
+                chunk = client.recv(4096)
+                if not chunk:
+                    break
+                received += chunk
+
+            sha = hashlib.sha256()
+            sha.update(received)
+            final_hash = sha.hexdigest()
+
+            if expected_hash != final_hash:
+                raise KeyError(f'Hashes diferentes - Esperado: {expected_hash} - Recebido: {final_hash}')
+
+            with open(file_name, 'wb') as f:
+                f.write(received)
+
+            print("Arquivo recebido com sucesso")
+
+    except Exception as e:
+        print(f'Erro na comunicação: {e}')
+        client.close()
+
+def send():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                client.send(nickname.encode())
+            message = input('Escolha o tipo de comunicação (Sair, Arquivo [file_name], Chat [Mensagem]): ')
+            if message.startswith('Sair'):
+                print('Encerrando comunicação com o servidor')
+                client.close()
+                break
+            elif message.startswith('Arquivo'):
+                    file = 'GET ' + message.split()[1]
+                    client.sendall(file.encode())
+                    receive('GET', message.split()[1])
+            elif message.startswith('Chat'):
+                    chat = 'MESSAGE ' + ''.join(message.split()[1:])
+                    client.sendall(chat.encode())
+                    receive('MESSAGE')
             else:
-                print(message)
-        except:
-            print('An error occurred')
-            client.close()
-            break
+                print(f'Método de comunicação inválido: {message}')
+                print('Opções são (Sair, Arquivo [file_name], Chat [Mensagem])')
+        except Exception as e:
+            print(f'Erro na comunicação com o servidor: {e}')
 
-def write():
-    while True:
-        message = f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))
-
-receive_thread = threading.Thread(target=receive)
+receive_thread = threading.Thread(target=send)
 receive_thread.start()
-write_thread = threading.Thread(target=write)
-write_thread.start()
--- server/server.py
+++ server/server.py
@@ -1,5 +1,8 @@
 import threading
 import socket
+import hashlib
+import os
+import struct
 
 IP_ADDRESS = 'localhost'
 PORT = 8000
@@ -7,43 +10,74 @@ PORT = 8000
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((IP_ADDRESS, PORT))
 server.listen()
-clients = []
-nicknames = []
 
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+messages = []
 
-def handle(client):
+def handle_file(client, file_name):
+    if not os.path.exists(file_name):
+        client.send('FILE_NOT_FOUND'.encode())
+        print(f'Arquivo {file_name} não encontrado')
+    client.send('OK_START_SENDING'.encode())
+
+    sha = hashlib.sha256()
+
+    size = os.path.getsize(file_name)
+
+    with open(file_name, 'rb') as f:
+        while True:
+            chunk = f.read(4096)
+            if not chunk:
+                break
+            sha.update(chunk)
+
+    hash = sha.hexdigest()
+
+    client.send(hash.encode())
+
+    client.send(struct.pack('!Q', size))
+
+    with open(file_name, 'rb') as f:
+        while True:
+            chunk = f.read(4096)
+            if not chunk:
+                break
+            client.sendall(chunk)    
+
+    print('Arquivo enviado: ' + file_name)
+    print('Hash: ' + hash)
+
+def handle_message(client, string):
+    messages.append(string)
+    response = '\n'.join(messages)
+    client.sendall(response.encode())
+    client.send('OK'.encode())
+    
+
+def handle(client, address):
     while True:
         try:
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            nicknames.remove(nickname)
-            broadcast(f'Nickname {nickname} left the chat'.encode('ascii'))
-            nicknames.remove(nickname)
-            break
+            message = client.recv(1024).decode('ascii')
+            if message.startswith('GET'):
+                file_name = message.split()[1]
+                handle_file(client, file_name)
+            elif message.startswith('MESSAGE'):
+                string = ' '.join(message.split()[1:])
+                handle_message(client, string)
+            else:
+                client.close()
+                print(f'Cliente {address} desconectado')
+                break
+        except Exception as e:
+            print(f'Erro interno do servidor: {e}')
+            client.send(''.encode())
 
 def receive():
     while True:
         client, address = server.accept()
-        print(f'Connected with {str(address)}')
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
+        print(f'Conectado com {str(address)}')
 
-        print(f'Nickname: {nickname}')
-        broadcast(f'{nickname} joined the chat'.encode('ascii'))
-        client.send('Connected to the server'.encode('ascii'))
-
-        thread = threading.Thread(target=handle, args=(client,))
+        thread = threading.Thread(target=handle, args=(client, address))
         thread.start()
 
-print('Server is listening...')
+print('Server ouvindo...')
 receive()
